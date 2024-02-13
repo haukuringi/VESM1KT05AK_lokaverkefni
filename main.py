8766df9647b964225c63901f7629db0dd9f31262
PeturAtla <159776433+PeturAtla@users.noreply.github.com>
2024-02-13 11:20:00 +0000

    main.py

--- main.py
+++ main.py
@@ -1,41 +1,63 @@
+from machine import Pin, PWM,
 
+from time import sleep_ms, ticks_ms
 
-import speech
+hatalari_passive = PWM(Pin(14), freq=1000)
+def lag():
+    hatalari_passive.duty(553)
+    hatalari_passive.freq(90)
+    sleep_ms(500)
+    hatalari_passive.freq(174)
+    sleep_ms(500)
+    hatalari_passive.freq(90)
+    sleep_ms(500)
+    hatalari_passive.freq(174)
+    sleep_ms(500)
+    hatalari_passive.freq(90)
+    sleep_ms(500)
+    hatalari_passive.freq(174)
+    sleep_ms(500)
+    hatalari_passive.freq(90)
+    sleep_ms(500)
+    hatalari_passive.freq(174)
+    sleep_ms(500)
+    hatalari_passive.freq(90)
+    hatalari_passive.duty(0)
+
 
 lives = 3
 
 while True:
 
-    display.scroll("You have " + str(lives) + " lives")
-
-    speech.say("You have " + str(lives) + " lives to operate on the patient. Good luck")
+    
 
     while lives > 0:
 
         if pin2.read_digital() == 1:
 
-            display.show(Image.ANGRY)
+          
 
             pin1.write_digital(1)
 
-            speech.say("Ouch!")
+            
             
             sleep(200)
 
             lives = lives -1
 
-            speech.say("You have " + str(lives) + " lives left.")
+            
 
             pin1.write_digital(0)
 
             pin2.write_digital(0)
 
         else:
+            lives = 3
 
-            display.show(Image.HAPPY)
+           
 
-    speech.say("GAME OVER")
+   
 
-    lives = 3
+    
     
     

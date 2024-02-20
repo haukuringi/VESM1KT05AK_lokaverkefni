4582a78eefae404fdf9b0b30d512b65a2fea5e75
Pétur Atlason <159776433+PeturAtla@users.noreply.github.com>
2024-02-20 11:43:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from machine import Pin, PWM,
 from time import sleep_ms, ticks_ms
 
 hatalari_passive = PWM(Pin(14), freq=1000)
+Pin2 = Pin(7)
+Pin1 = Pin(8)
 def lag():
     hatalari_passive.duty(553)
     hatalari_passive.freq(90)
@@ -33,27 +35,15 @@ while True:
 
     while lives > 0:
 
-        if pin2.read_digital() == 1:
-
-          
-
-            pin1.write_digital(1)
-
-            
-            
+        if Pin2.value() == 1:
+            Pin1.value(1)
             sleep(200)
-
             lives = lives -1
-
-            
-
-            pin1.write_digital(0)
-
-            pin2.write_digital(0)
+            Pin1.value(0)
+            Pin2.value(0)
 
         else:
             lives = 3
-
            
 
    

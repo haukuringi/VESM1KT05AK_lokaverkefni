8b00ef6ce8a3d0e5e6353b66d756f103e23d2557
nonniblonni <113126611+nonniblonni@users.noreply.github.com>
2024-02-26 11:08:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from machine import Pin, PWM,
-
+from machine import Pin, PWM
 from time import sleep_ms, ticks_ms
+from neopixel import NeoPixel
+
+neo = NeoPixel(Pin(5),8)
+
+button_pin = 4  
+led_pin = 5     
+
+Initialize GPIO pins
+button = Pin(button_pin, Pin.IN, Pin.PULL_UP)
+led = Pin(led_pin, Pin.OUT)
+
 
 hatalari_passive = PWM(Pin(14), freq=1000)
 Pin2 = Pin(7)
@@ -44,10 +54,12 @@ while True:
 
         else:
             lives = 3
-           
+    
+    if button.value() == 0:
+        led.on()    
+        hatalari_passive.freq(180) 
+    else:
+        led.off()   
 
-   
+    time.sleep(0.1)       
 
-    
-    
-    

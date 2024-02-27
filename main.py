fc6e22d9afab25508ecb8ca11357278c449a35ad
Pétur Atlason <159776433+PeturAtla@users.noreply.github.com>
2024-02-27 11:45:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ led_pin = 5
 
 button = Pin(button_pin, Pin.IN, Pin.PULL_UP)
 led = Pin(led_pin, Pin.OUT)
-
+alpappir = Pin(button_pin, Pin.IN, Pin.PULL_UP)
 
 hatalari_passive = PWM(Pin(14), freq=1000)
 #pin3=red pin2=græn pin1=gulur
 Pin3 = Pin(12, Pin.OUT)
 Pin2 = Pin(11, Pin.OUT)
 Pin1 = Pin(10, Pin.OUT)
+
 def lag():
     hatalari_passive.duty(511)
     hatalari_passive.freq(90)
@@ -40,23 +41,23 @@ def lag():
     hatalari_passive.duty(0)
 
 
-lives = 3
-
+    lives = 3
+def slokkva():
+    Pin1.value(0)
+    Pin2.value(0)
+    Pin3.value(0)
 while True:
 
-    if alfpappir.value() == 1:
+    if alpappir.value() == 1:
         slokkva()
-        pin3.value(1)
+        Pin3.value(1)
         start = True
         lag()
         
-    if alfpappir.value() == 0:
+    if alpappir.value() == 0:
         slokkva()
-        pin2.value(1)
+        Pin2.value(1)
         start = True
         lag()
         
-    if Pin1.value() == 1:
-
-
 
